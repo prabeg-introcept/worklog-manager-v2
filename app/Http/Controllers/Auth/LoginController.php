d2c80478b9035f47ccf7efcfd71a3af62f2a059b
prabeg-introcept <prabeg.shakya@introcept.co>
2021-11-16 12:58:56 +0545

    Complete user login feature
    - Add routes for user login, logout, and worklog dashboard with
      middleware
    - Add form validation for login
    - Add view for login and worklog dashboard
    - Save user session, redirect to dashboard on success, and logout user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -14,5 +18,31 @@ class LoginController extends Controller
     public function create()
     {
         return view('auth.login');
+    }
+
+    /**
+     * @param LoginUserRequest $request
+     * @return RedirectResponse
+     */
+    public function store(LoginUserRequest $request)
+    {
+        if(!Auth::attempt($request->validated()))
+        {
+            return back()
+                ->with(['userLoginStatus' => 'Invalid credentials! Please try again.']);
+        }
+        $request->session()->regenerate();
+
+        return redirect()->route('worklogs.index');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
