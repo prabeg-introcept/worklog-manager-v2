5c38c3489cbda3f58e4003c623c71614000130cf
prabeg-introcept <prabeg.shakya@introcept.co>
2021-11-18 16:00:35 +0545

    Inject worklog dependancy, use the throw_if helper, and clean methods name

--- app/Services/WorklogService.php
+++ app/Services/WorklogService.php
@@ -7,67 +7,72 @@ use App\Exceptions\Worklogs\WorklogNotCreatedException;
 use App\Exceptions\Worklogs\WorklogNotFoundException;
 use App\Exceptions\Worklogs\WorklogNotUpdatedException;
 use App\Models\Worklog;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Auth;
+use Throwable;
 
 class WorklogService
 {
+    private Worklog $worklog;
+
+    public function __construct(Worklog $worklog)
+    {
+        $this->worklog = $worklog;
+    }
+
     /**
      * @return mixed
-     * @throws WorklogNotFoundException
+     * @throws Throwable
      */
     public function getLoggedInUserWorklogs(): Collection
     {
-        $worklogs = Worklog::where('user_id', Auth::id())
-            ->orderBy('created_at', 'desc')
-            ->get();
-        if(!$worklogs)
-        {
-            throw new WorklogNotFoundException('No worklogs created yet. Click Add Task to create new worklog');
-        }
+        $worklogs = auth()->user()->worklogs;
+
+        throw_if(!$worklogs,
+            WorklogNotFoundException::class,
+        FlashMessages::ERROR_FETCH_WORKLOG
+        );
+
         return $worklogs;
     }
 
     /**
      * @param $worklogId
-     * @return Collection
+     * @return Worklog
      */
-    public function getWorklog($worklogId): Worklog
+    public function find(int $worklogId): Worklog
     {
-        return Worklog::findOrFail($worklogId);
+        return $this->worklog->findOrFail($worklogId);
     }
 
     /**
      * @param array $validatedWorklogData
-     * @throws WorklogNotCreatedException
+     * @throws Throwable
      */
-    public function createWorklog(array $validatedWorklogData): void
+    public function create(array $validatedWorklogData): void
     {
-        if(Worklog::create($validatedWorklogData))
-        {
-            throw new WorklogNotCreatedException(FlashMessages::ERROR_CREATE_WORKLOG);
-        }
+        throw_if(!$this->worklog->create($validatedWorklogData),
+        WorklogNotCreatedException::class,
+            FlashMessages::ERROR_CREATE_WORKLOG
+        );
     }
 
     /**
      * @param array $validatedWorklogData
-     * @param $worklogId
-     * @throws WorklogNotUpdatedException
+     * @param int $worklogId
+     * @throws Throwable
      */
-    public function updateWorklog(array $validatedWorklogData, $worklogId): void
+    public function update(array $validatedWorklogData, int $worklogId): void
     {
-        $worklog = $this->getWorklog($worklogId);
+        $worklog = $this->find($worklogId);
 
-        $dateCreated = Carbon::now()->format('Y-m-d');
-        $dateToday = $worklog->created_at->format('Y-m-d');
+        throw_if(!$worklog->created_at->isToday(),
+            WorklogNotUpdatedException::class,
+            FlashMessages::ERROR_UPDATE_WORKLOG_ON_DIFFERENT_DATE
+        );
 
-        if($dateCreated !== $dateToday){
-            throw new WorklogNotUpdatedException(FlashMessages::ERROR_UPDATE_WORKLOG_ON_DIFFERENT_DATE);
-        }
-
-        if(!$worklog->update($validatedWorklogData)){
-            throw new WorklogNotUpdatedException(FlashMessages::ERROR_UPDATE_WORKLOG);
-        }
+        throw_if(!$worklog->update($validatedWorklogData),
+            WorklogNotUpdatedException::class,
+            FlashMessages::ERROR_UPDATE_WORKLOG
+        );
     }
 }
