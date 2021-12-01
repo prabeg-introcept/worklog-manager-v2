cf0c6e83b5cce3597bf1c2e917ff02634f306652
prabeg-introcept <prabeg.shakya@introcept.co>
2021-12-01 15:14:03 +0545

    Register policy for worklog

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Worklog;
+use App\Policies\WorklogPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Worklog::class => WorklogPolicy::class
     ];
 
     /**
